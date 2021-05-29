6eca3c9b44d7f30bd51a3cc5afe3484d7e11fee2
francisco.ferrandez <francisco.ferrandez@nunsys.com>
2021-05-29 18:29:30 +0200

    Domain definition

---
+++ Domain/Entities/Issue.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Issue : AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Note { get; set; }
+
+        public IssueSeverity Severity { get; set; }
+
+        public IssueStatus Status { get; set; }
+
+
+    }
+}
